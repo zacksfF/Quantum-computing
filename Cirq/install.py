1e11e6665731a316450c3b7043688de7d2273b70
zakaria saif <129240583+zacksfF@users.noreply.github.com>
2023-09-24 15:13:25 +0100

    Add files via upload

---
+++ Cirq/install.py
@@ -0,0 +1,18 @@
+# pip install cirq
+# run my first program
+# let's start build some prject with cirq
+
+import cirq
+
+qubit = cirq.GridQubit(0, 0)
+
+# Create a circuit that applies a square root of NOT gate, then measures the qubit.
+circuit = cirq.Circuit(cirq.X(qubit) ** 0.5, cirq.measure(qubit, key='m'))
+print("Circuit:")
+print(circuit)
+
+# Simulate the circuit several times.
+simulator = cirq.Simulator()
+result = simulator.run(circuit, repetitions=20)
+print("Results:")
+print(result)

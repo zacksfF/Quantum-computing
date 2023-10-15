452a34c1366099d100e0e02a95812303f303d195
zakaria saif <129240583+zacksfF@users.noreply.github.com>
2023-10-15 17:46:47 +0100

    Add files via upload

---
+++ Qiskit files/mod1.py
@@ -0,0 +1,4 @@
+def module1Function():
+    print("This function is in module 1")
+    
+print("Module 1 has been loaded")
---
+++ Qiskit files/mod2.py
@@ -0,0 +1,4 @@
+def module2Function():
+    print("This function is in module 2")
+    
+print("Module 2 has been loaded")
